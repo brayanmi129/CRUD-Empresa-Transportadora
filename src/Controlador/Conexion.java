cd59a814514b90f9a930ec2f2361a6767156b60a
TheCrixS <csotov@ucentral.edu.co>
2025-03-13 11:41:28 -0500

    Agregando cambios pendientes y archivos nuevos

---
+++ src/Controlador/Conexion.java
@@ -0,0 +1,29 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ts3plus
+ */
+public class Conexion {
+    Connection conectar = null;
+    //Cadena de conexion a la base de datos
+    String cadena_conexion = "jdbc:sqlite:EmpresaTransporte.db";
+    
+    //Metodo establecer conexion
+    public Connection establecerConexion(){
+        conectar = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            //Abrir conexion
+            conectar = DriverManager.getConnection(cadena_conexion);
+            System.out.println("Conexion Exitosa");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error: No se realizo la conexion " + e.getMessage());
+        }
+        return conectar;
+    }
+}
