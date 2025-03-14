85a7632ba75d4c5b5a4781165ad33d5691a5cf60
TheCrixS <csotov@ucentral.edu.co>
2025-03-14 09:11:06 -0500

    CRUD entidades Bulto y Conductor añadidas

---
+++ src/Controlador/Conductor/cConsultar.java
@@ -0,0 +1,55 @@
+package Controlador.Conductor;
+
+import Controlador.Conexion;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Cristopher Soto Viloria
+ */
+public class cConsultar {
+    //Metodo para traer los datos de la base de datos
+    public void mostrarConductor(JTable tblConductor){
+        //Objeto de conexion a la base de datos
+        Conexion conexion = new Conexion();
+        //Onjeto para manejar la tabla
+        DefaultTableModel modelo = new DefaultTableModel();
+        //Variable para instrcuccion sql
+        String sql = "";
+        //Titulos de la tabla
+        modelo.addColumn("CC");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Apellidos");
+        modelo.addColumn("Sueldo");
+        modelo.addColumn("Antiguedad");
+        //Llevar los titulos a la tabla
+        tblConductor.setModel(modelo);
+        //Instruccion SQL
+        sql = "SELECT * FROM Conductor";
+        //Vector para los registros}
+        String [] datos = new String[5];
+        //Variable tipo statement
+        Statement st;
+        try{
+            st = conexion.establecerConexion().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            //Recorrer el paquete de datos y manipularlo registro a registro
+            while(rs.next()){
+                datos[0] = rs.getString(1);
+                datos[1] = rs.getString(2);
+                datos[2] = rs.getString(3);
+                datos[3] = rs.getString(4);
+                datos[4] = rs.getString(5);
+                //Adicionar registro a el modelo
+                modelo.addRow(datos);
+            }
+            tblConductor.setModel(modelo);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Error: No se genero el resultado " + e.getMessage());
+        }
+    }
+}
---
+++ src/Modelo/Bulto/cActualizar.java
@@ -0,0 +1,32 @@
+package Modelo.Bulto;
+
+import Controlador.Conexion;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cristopher Soto Viloria
+ */
+public class cActualizar {
+    public void actualizarBulto(int ID_Bulto, String remitente, String destinatario, String valora, String valorf){
+        //Crear conexion con la base de datos
+        Conexion conexion = new Conexion();
+        //Variable sql
+        String sql = "";
+        //Instruccion Sql
+        sql = "UPDATE Bulto SET Remitente = ?, Destinatario = ?, Valor_asegurado = ?, Precio_pagado_por_traslado = ? WHERE ID_Bulto = ?";
+        try{
+            PreparedStatement pst = conexion.establecerConexion().prepareStatement(sql);
+            pst.setString(1, remitente);
+            pst.setString(2, destinatario);
+            pst.setString(3, valora);
+            pst.setString(4, valorf);
+            pst.setInt(5, ID_Bulto);
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "El registro se actualizo correctamente");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error no se pudo actualizar el registro" + e.getMessage());
+        }
+    }
+}
